5d27a16e4764ba2d24de2d2234e37312a5cdb4e3
manik <manikronty0@gmail.com>
2024-06-01 00:02:53 +0530

    img alt chane

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,7 +7,7 @@ const Card = ({ title, image, children }) => {
         <div className="mb-4">
           <img
             src={image}
-            alt=""
+            alt="sam94"
             className="w-full h-48 object-cover rounded-lg"
           />
         </div>
